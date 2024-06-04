c786d0a3cef236404004df16cfbee7cf19d0a01a
L31T1NH0 <oLeiteG@hotmail.com>
2024-06-04 02:29:05 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   theme: {
     extend: {
       colors: {
-        background: '#181A1B',
-        primary: '#E8E6E3',
-        secondary: '#A8A095',
-        link: '#3AA4FF'
-      }
-    }
+        background: "#040404",
+        primary: "#E8E6E3",
+        secondary: "#A8A095",
+        link: "#3AA4FF",
+      },
+    },
   },
   plugins: [
-    ({ addVariant }) => addVariant('inner', '& > *'),
-    require("tailwind-scrollbar")
-  ]
+    ({ addVariant }) => addVariant("inner", "& > *"),
+    require("tailwind-scrollbar"),
+  ],
 };
