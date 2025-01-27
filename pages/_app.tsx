5a1dbe6355bf6dadd8db1084d65171c50be5f08d
Domenyk <93108779+L31T1NH0@users.noreply.github.com>
2025-01-27 06:26:04 -0300

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from "next/app";
 import "../styles/global.css";
 import { JSX } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
