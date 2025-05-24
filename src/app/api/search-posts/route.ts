01d87d030e485cccc534969ce69a415f42f95f85
Domenyk <93108779+L31T1NH0@users.noreply.github.com>
2025-05-24 14:57:46 -0300

    Update route.ts

--- src/app/api/search-posts/route.ts
+++ src/app/api/search-posts/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: Request) {
     const db = client.db("blog");
     const postsCollection = db.collection("posts");
 
-    // Busca posts
+    // Busca posts com projeção para retornar apenas os campos necessários
     let posts;
     if (query.trim() === "") {
       // Retorna todos os posts se a query estiver vazia
@@ -18,6 +18,14 @@ export async function GET(req: Request) {
         .find({})
         .sort({ date: -1 })
         .limit(10)
+        .project({
+          postId: 1,
+          title: 1,
+          date: 1,
+          views: 1,
+          tags: 1,
+          _id: 0, // Exclui o campo _id
+        })
         .toArray();
     } else {
       // Busca com base na query
@@ -30,6 +38,14 @@ export async function GET(req: Request) {
         })
         .sort({ date: -1 })
         .limit(10)
+        .project({
+          postId: 1,
+          title: 1,
+          date: 1,
+          views: 1,
+          tags: 1,
+          _id: 0, // Exclui o campo _id
+        })
         .toArray();
     }
 
