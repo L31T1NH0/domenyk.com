9efb5f3a2670ae148fa7ad7752209d42efa059de
Domenyk <93108779+L31T1NH0@users.noreply.github.com>
2025-11-11 06:11:28 -0300

    Add scroll progress indicator

--- components/layout.tsx
+++ components/layout.tsx
@@ -13,10 +13,14 @@ type LayoutProps = {
 export function Layout({ home = false, children }: LayoutProps) {
   return (
     <div className="max-w-xl flex flex-col mx-auto px-4 mb-4">
-      <div className="flex justify-between items-center py-1">
-      <ThemeSwitcher /> {/* Botão de brilho à direita */}
-      <SettingsMenu /> {/* Botão de três pontos à esquerda */}
-      </div>
+      <header
+        data-scroll-progress-root
+        className="flex justify-between items-center py-1"
+      >
+        <div aria-hidden="true" data-scroll-progress-bar />
+        <ThemeSwitcher /> {/* Botão de brilho à direita */}
+        <SettingsMenu /> {/* Botão de três pontos à esquerda */}
+      </header>
       <main className={`${home ? "home" : ""} flex flex-col flex-1`}>
         {children}
       </main>
