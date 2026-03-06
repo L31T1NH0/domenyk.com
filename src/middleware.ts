e987086e6f38e3c6ce32b4756eb40460574b0051
Domenyk <93108779+L31T1NH0@users.noreply.github.com>
2026-03-06 11:58:59 -0300

    Restore original middleware matcher config

--- src/middleware.ts
+++ src/middleware.ts
@@ -77,7 +77,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon\\.ico).*)",
-    "/api/(.*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2|ico|csv|docx?|xlsx?|zip|webmanifest)|public/.*).*)",
+    "/(api/auth|api/clerk)(.*)",
   ],
 };
